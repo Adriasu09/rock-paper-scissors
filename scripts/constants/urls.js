8191d0638be69758770dee351f11597b60dffc23
Adriasu <adsuarez09@gmail.com>
2026-04-20 11:04:51 +0200

    refactor: migrate NewsAPI from top-headlines to /v2/everything endpoint, replacing country/category params with keyword queries per category

--- scripts/constants/urls.js
+++ scripts/constants/urls.js
@@ -2,4 +2,4 @@ export const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/reverse";
 export const FLAG_BASE_URL = "https://flagcdn.com/w160";
 export const OPEN_METEO_BASE_URL = "https://api.open-meteo.com/v1/forecast";
 export const WEATHER_ICON_BASE_URL = "https://openweathermap.org/img/wn";
-export const NEWSAPI_BASE_URL = "https://newsapi.org/v2/top-headlines";
+export const NEWSAPI_BASE_URL = "https://newsapi.org/v2/everything";
